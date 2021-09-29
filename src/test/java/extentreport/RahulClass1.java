652bf0190a85f28bba4961b7d2a8219dafcce33f
kulkarnirahul002 <kulkarnirahul002@gmail.com>
2021-09-29 18:27:26 +0530

    commiting to rahul branch

--- src/test/java/extentreport/RahulClass1.java
+++ src/test/java/extentreport/RahulClass1.java
@@ -3,6 +3,10 @@ package extentreport;
 public class RahulClass1 {
 	public static void main(String[] args) {
 		System.out.println("For Rahul");
+		
+		System.out.println("For Rahul");
+		System.out.println("For Rahul");
+		System.out.println("For Rahul");
 	}
 
 
